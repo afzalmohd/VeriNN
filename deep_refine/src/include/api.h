2226b6d3a40105e03d158c45ba482cfa210b6832
ASHUTOSH GUPTA <akg@iitb.ac.in>
2021-03-12 01:13:22 +0530

    dsadsa

---
+++ deep_refine/src/include/api.h
@@ -0,0 +1,17 @@
+#ifndef DREFINE_API_H
+#define DREFINE_API_H
+
+// my api function declaration here
+
+// minimum declarations here such that user can use your library
+
+class NN{
+  
+};
+
+
+void initialize_drefine( std::string nn_file_name, std::string eran_out_trace );
+
+void run_drefine();
+
+#endif
