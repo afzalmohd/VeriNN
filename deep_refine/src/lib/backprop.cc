cb5362280928b462bf14709fc458715cb3aeb9ed
Mohammad Afzal <afzal7242mohd@gmail.com>
2021-04-14 15:49:20 +0530

    cleaning

--- deep_refine/src/lib/backprop.cc
+++ deep_refine/src/lib/backprop.cc
@@ -56,14 +56,3 @@ void back_substitute(z3::context& c, Network_t* net){
     }
 }
 
-
-// int main(){
-//     std::string filepath = "/home/u1411251/Documents/Phd/tools/ERAN/tf_verify/fppolyForward.txt";
-//     Network_t* net = new Network_t();
-//     z3::context c;
-//     init_network(c,net,filepath);
-//     set_predecessor_and_z3_var(c,net);
-//     init_z3_expr(c,net);
-//     printf("\nCheck..\n");
-//     return 0;
-// }
--- deep_refine/src/main.cpp
+++ deep_refine/src/main.cpp
@@ -3,30 +3,7 @@
 
 int main(int argc, char* argv[]){
 
-  // cmd line processing code here
-  // std::string path = "/home/u1411251/Documents/Phd/tools/VeriNN/deep_refine/";
-  
-  // bool is_my_test = false;
-
-  // std::string path = "";
-  // std::string filepath = "";
-  // std::string net_path = "";
-
-  // if(is_my_test){
-  //   filepath = path+"benchmarks/fppolyForward_my_test.txt"; // output of eran
-  //   net_path = path+"benchmarks/mnist_my_text.tf"; //small test case
-  // }
-  // else{
-  //   filepath = path+"benchmarks/fppolyForward.txt"; // output of eran
-  //   net_path = path+"benchmarks/mnist_relu_3_50.tf"; // network to be verified
-  // }
-  // std::string dataset_path = path+"benchmarks/mnist_test.csv"; // image file
-  find_refine_nodes(argc, argv);
+find_refine_nodes(argc, argv); //argv are the arguments passes like abstraction output, neural network file,
+                               // image dataset file, epsilon etc. 
   return 0;
 }
-
-//   initialize_drefine();
-//   markings = run_drefine();
-//   // print markings
-//   return 0;
-// }
