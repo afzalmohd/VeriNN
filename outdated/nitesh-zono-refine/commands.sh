cb35f0ae7565be125bfcb072ed929a605de6cf62
afzalmohd <afzal7242mohd@gmail.com>
2023-07-26 14:26:47 +0530

    removed..

--- outdated/nitesh-zono-refine/commands.sh
+++
@@ -1,3 +0,0 @@
-python3 modifier.py input_unmodified.txt input_modified.txt
-python3 modifier.py internal_unmodified.txt internal_modified.txt
-python3 modifier.py output_unmodified.txt output_modified.txt
