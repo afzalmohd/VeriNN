c42e3039057778ac45815470f7065c2916f54820
Jawwadabd <Jawwadabd@github.com>
2024-02-02 00:36:04 +0530

    asd

--- deep_refine/run_tool.sh
+++ deep_refine/run_tool.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
-num_cpu=8
+num_cpu=1
 root_dir=`pwd`
 log_dir=$root_dir/logs
 rm -rf $log_dir
 # result_dir=$root_dir/results
 # result_dir=$result_dir/mnistdeeppoly.csv
 # export RESULT_FILE=$result_dir
-python script.py $num_cpu $log_dir
+python script_jkp.py $num_cpu $log_dir
 chmod -R u+x $log_dir/
 
 for((i=0; i<$num_cpu; i++))
