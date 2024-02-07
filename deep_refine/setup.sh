a6d55acbd36bbc0c7392def362822b86e14f29ed
Jawwadabd <Jawwadabd@github.com>
2024-02-07 18:49:57 +0530

    minor

--- deep_refine/setup.sh
+++ deep_refine/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+curl --location-trusted -u 22m0741:4e31608f35f5430d2a5bf3e17af8b45d "https://internet-sso.iitb.ac.in/login.php" > /dev/null
 # Activate conda environment
 source ~/anaconda3/bin/activate
 conda activate verinn
