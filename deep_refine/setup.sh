fa9680c6fbb5293e4e9c8b4d090123bb87896fd6
Jawwadabd <Jawwadabd@github.com>
2024-02-07 18:48:06 +0530

    setup file

---
+++ deep_refine/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Activate conda environment
+source ~/anaconda3/bin/activate
+conda activate verinn
+
+# Set project directory and binary name
+PROJECT_DIR=/home/afzal/tools/VeriNN
+BIN_NAME=drefine
+
+# Set LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${PROJECT_DIR}/deep_refine/ex_tools/boost_1_68_0/installed/lib:${PROJECT_DIR}/deep_refine/ex_tools/gurobi912/linux64/lib:/home/afzal/anaconda3/envs/verinn/lib
+
+
+
